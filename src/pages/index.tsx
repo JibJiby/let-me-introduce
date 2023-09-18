cf7e704ad03f84aeb5fea78239b35d903ea5ccab
jibjiby <my01sun@gmail.com>
2023-09-18 17:50:06 +0900

    style: 기본 레이아웃 세팅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
 import { Inter } from 'next/font/google'
 
-import { Button } from '@/components/ui/button'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <Button>버튼</Button>
+    <main className={`flex h-full flex-col ${inter.className}`}>
+      <div className="flex items-center justify-center">jiby</div>
+      <div className="min-h-[60px] space-x-4 absolute bottom-0 flex flex-row w-full border">
+        <div>a</div>
+        <div>b</div>
+        <div>c</div>
+      </div>
     </main>
   )
 }
