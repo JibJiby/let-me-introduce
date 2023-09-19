407a8e3ebe374b1e9f5602c3b038befc6b282a20
jibjiby <my01sun@gmail.com>
2023-09-19 16:12:30 +0900

    fix: tooltip style opacity 관련 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,10 +38,12 @@ const icons = [
 export default function Home() {
   return (
     <main className={`flex h-full flex-col ${inter.className}`}>
-      <div className="flex items-center justify-center select-none">jiby</div>
-      <div className="min-h-[60px] fixed bottom-2 w-full px-6 select-none flex justify-center">
-        <footer
-          className="flex flex-row w-full h-full max-w-lg px-4 py-3 space-x-4 rounded-2xl justify-evenly"
+      <section className="flex items-center justify-center w-full h-full select-none">
+        jiby
+      </section>
+      <footer className="min-h-[60px] fixed bottom-2 w-full px-6 select-none flex justify-center">
+        <div
+          className="flex flex-row w-full h-full max-w-md px-4 py-3 rounded-2xl justify-evenly"
           style={{
             backgroundColor: 'rgba(51, 51, 51, 0.4)',
             boxShadow: 'rgba(0, 0, 0, 0.4) 0px 30px 90px',
@@ -52,6 +54,7 @@ export default function Home() {
               key={icon.src}
               delayDuration={0}
               disableHoverableContent
+              skipDelayDuration={300}
             >
               <Tooltip>
                 <TooltipTrigger asChild>
@@ -66,14 +69,14 @@ export default function Home() {
                     </IconBox>
                   </a>
                 </TooltipTrigger>
-                <TooltipContent>
-                  <strong>{icon.tooltip}</strong>
+                <TooltipContent className="mb-2 opacity-80">
+                  <strong className="opacity-80">{icon.tooltip}</strong>
                 </TooltipContent>
               </Tooltip>
             </TooltipProvider>
           ))}
-        </footer>
-      </div>
+        </div>
+      </footer>
     </main>
   )
 }
@@ -83,7 +86,7 @@ type Props = React.ComponentProps<'div'>
 function IconBox({ children }: Props) {
   return (
     <div
-      className="p-2 transition-all duration-150 ease-in bg-white rounded-lg cursor-pointer hover:scale-125"
+      className="p-2 transition-all duration-150 ease-in bg-white rounded-lg cursor-pointer select-none hover:scale-125"
       style={{
         boxShadow:
           'rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px',
