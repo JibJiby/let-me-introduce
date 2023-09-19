abb47d05a38e519aa8a55cf0021f0487c6f7c467
jibjiby <my01sun@gmail.com>
2023-09-19 16:43:24 +0900

    refactor: icon-box 컴포넌트 분리

---
+++ src/components/icon-box.tsx
@@ -0,0 +1,15 @@
+type Props = React.ComponentProps<'div'>
+
+export function IconBox({ children }: Props) {
+  return (
+    <div
+      className="p-2 transition-all duration-150 ease-in bg-white rounded-lg cursor-pointer select-none hover:scale-125"
+      style={{
+        boxShadow:
+          'rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px',
+      }}
+    >
+      {children}
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,8 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip'
 
+import { IconBox } from '@/components/icon-box'
+
 const inter = Inter({ subsets: ['latin'] })
 
 /**
@@ -104,21 +106,5 @@ export default function Home() {
         </div>
       </footer>
     </main>
-  )
-}
-
-type Props = React.ComponentProps<'div'>
-
-function IconBox({ children }: Props) {
-  return (
-    <div
-      className="p-2 transition-all duration-150 ease-in bg-white rounded-lg cursor-pointer select-none hover:scale-125"
-      style={{
-        boxShadow:
-          'rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px',
-      }}
-    >
-      {children}
-    </div>
   )
 }
