6d585e5035b7effe0aa6d23daa0ef1c28a905716
jibjiby <my01sun@gmail.com>
2023-09-20 17:10:41 +0900

    chore: next-themes 패키지 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from 'next/app'
 
+import { ThemeProvider } from '@/components/theme-provider'
+
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
